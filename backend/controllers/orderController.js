3cf811194bb1dae0b744955a62756547861c6ed6
AnoopKrishnan R <anoop4u777@gmail.com>
2024-07-16 11:33:38 -0400

    Final commit with all changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -94,4 +94,15 @@ const listOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders, listOrders};
+// Api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+        res.json({success:true, message: "Status updated"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Unable update status"});
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus};
