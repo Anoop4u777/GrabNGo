5d4ee6aa34903a8c3aa31354d6b33aa8e4558ae7
AnoopKrishnan R <anoop4u777@gmail.com>
2024-07-16 11:12:34 -0400

    changes: added order details in admin page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -83,4 +83,15 @@ const userOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders};
+// Getting orders for admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true, data: orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Unable to fetch order data"});
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders};
